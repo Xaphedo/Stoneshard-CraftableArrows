cab422983cabe5615bead681f8132b9f2b15f352
Xaphedo <21255475+Xaphedo@users.noreply.github.com>
2024-09-04 11:54:48 +0200

    Update CharredTipArrows.cs
    added new IDs for bladed weapons in the context menu, added translations for making ammo log,  added asm for charred-tip bolts

--- CharredTipArrows.cs
+++ CharredTipArrows.cs
@@ -106,7 +106,7 @@ namespace CharredTipArrows
     {
         string findtext = "\";context_menu_end;context_menu_end;context_menu_end;context_menu_end;context_menu_end;context_menu_end;context_menu_end;context_menu_end;context_menu_end;context_menu_end;context_menu_end;context_menu_end;\""; //the string that the iterator is looking for
 
-        string inserttext = "\"101;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;Improvise some Arrows;\", \"102;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;Improvise some Bolts;\", \"103;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;You don't have any sticks;\","; //the string that the iterator will insert
+        string inserttext = "\"101;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;Make charred-tip arrows;\", \"102;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;Make charred-tip bolts;\", \"103;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;You have no bladed weapon and no sticks;\", \"104;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;You have a bladed weapon but no sticks;\", \"105;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;You have a stick but no bladed weapon;\","; //the string that the iterator will insert
 
         foreach(string item in input)
         {
@@ -146,7 +146,7 @@ namespace CharredTipArrows
     {
         string findtext = "\"cook;"; //the string that the iterator is looking for
 
-        string inserttext = "\"charredAmmo;~w~$~/~ готовит:;~w~$~/~ makes:;~w~$~/~做了：;~w~$~/~ kocht:;~w~$~/~ cocina:;~w~$~/~ prépare :;~w~$~/~ cucina:;~w~$~/~ cozinhou:;~w~$~/~ gotuje:;~w~$~/~ pişirdi:;~w~$~/~ は調理した：;~w~$~/~은(는) 요리했다 :;\", "; //the string that the iterator will insert
+        string inserttext = "\"charredAmmo;~w~$~/~ создает:;~w~$~/~ makes:;~w~$~/~制作：;~w~$~/~ erstellt:;~w~$~/~ crea:;~w~$~/~ crée :;~w~$~/~ crea:;~w~$~/~ cria:;~w~$~/~ tworzy:;~w~$~/~ yaratır:;~w~$~/~ は作ります：;~w~$~/~는 만듭니다 :;\", "; //the string that the iterator will insert
 
         foreach(string item in input)
         {
@@ -190,6 +190,11 @@ dup.v 0
 push.s ""Make_Charred_Arrows""
 cmp.s.v EQ
 bt [1000]
+
+dup.v 0
+push.s ""Make_Charred_Bolts""
+cmp.s.v EQ
+bt [1010]
 ";
             }
             else if (jmp_fill != "" && item.Contains(jmp_fill))
@@ -250,57 +255,62 @@ popz.v
 
 :[1006]
 popenv [1001]
+b {jmp_end}
+
+:[1010]
+push.v self.interact_id
+pushi.e -9
+pushenv [1016]
+
+:[1011]
+push.v self.in_grid
+push.v self.id
+call.i gml_Script_scr_can_interract_posgrid(argc=2)
+conv.v.b
+bf [1013]
+
+:[1012]
+pushi.e 2
+conv.i.v
+pushi.e 1
+conv.i.v
+pushi.e o_inv_charredtip_bolts
+conv.i.v
+call.i gml_Script_scr_makeCharredAmmo(argc=3)
+popz.v
+b [1016]
+
+:[1013]
+pushi.e 2
+conv.i.v
+pushi.e 1
+conv.i.v
+pushi.e o_inv_charredtip_bolts
+conv.i.v
+push.i gml_Script_scr_makeCharredAmmo
+conv.i.v
+call.i @@NewGMLArray@@(argc=4)
+pop.v.v self.interract_event
+push.v self.in_grid
+conv.v.b
+not.b
+bf [1015]
+
+:[1014]
+call.i gml_Script_scr_delay_move(argc=0)
+popz.v
+b [1016]
+
+:[1015]
+call.i gml_Script_scr_delay_move_grid(argc=0)
+popz.v
+
+:[1016]
+popenv [1011]
 b {jmp_end}";
             }
         }
     }
-
-
-
-
-
-/*
-
-    private static IEnumerable<string> SpeechIterator0(IEnumerable<string> input)
-    {
-        string findtext = "\"Ambush;Ambush;Ambush;Ambush;Ambush;Ambush;Ambush;Ambush;Ambush;Ambush;Ambush;Ambush;Ambush;"; //the string that the iterator is looking for
-        
-        string inserttext = "\"interruption_East;interruption_East;interruption_East;interruption_East;interruption_East;interruption_East;interruption_East;interruption_East;interruption_East;interruption_East;interruption_East;interruption_East;interruption_East\", \";Враг на Востоке!;Enemy to the East!;有敌人在东边！;Feind im osten!;¡Enemigo al este!;Ennemi à l'est !;Nemico ad est!;Inimigo ao leste!;Wróg na wschodzie!;Düşman doğuda!;東の敵だ！ / 東の敵よ！;동쪽이다 적!\", \"interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end;interruption_East_end\", \"interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast;interruption_NorthEast\", \";Враг на Северо-Востоке!;Enemy to the North-East!;有敌人在东北边！;Feind im nordosten!;¡Enemigo al noreste!;Ennemi au nord-est !;Nemico a nord-est!;Inimigo ao nordeste!;Wróg na północnym wschodzie!;Düşman kuzeydoğuda!;北東の敵だ！ / 北東の敵よ！;북동쪽이다 적!\", \"interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end;interruption_NorthEast_end\", \"interruption_North;interruption_North;interruption_North;interruption_North;interruption_North;interruption_North;interruption_North;interruption_North;interruption_North;interruption_North;interruption_North;interruption_North;interruption_North\", \";Враг на Севере!;Enemy to the North!;有敌人在北边！;Feind im norden!;¡Enemigo al norte!;Ennemi au nord !;Nemico a nord!;Inimigo ao norte!;Wróg na północy!;Düşman kuzeyde!;北の敵だ！ / 北の敵よ！;북쪽이다 적!\", \"interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end;interruption_North_end\", \"interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest;interruption_NorthWest\", \";Враг на Северо-Западе!;Enemy to the North-West!;有敌人在西北边！;Feind im nordwesten!;¡Enemigo al noroeste!;Ennemi au nord-ouest !;Nemico a nord-ovest!;Inimigo ao noroeste!;Wróg na północnym zachodzie!;Düşman kuzeybatıda!;北西の敵だ！ / 北西の敵よ！;북서쪽이다 적!\", \"interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end;interruption_NorthWest_end\", \"interruption_West;interruption_West;interruption_West;interruption_West;interruption_West;interruption_West;interruption_West;interruption_West;interruption_West;interruption_West;interruption_West;interruption_West;interruption_West\", \";Враг на Западе!;Enemy to the West!;有敌人在西边！;Feind im westen!;¡Enemigo al oeste!;Ennemi à l'ouest !;Nemico ad ovest!;Inimigo ao oeste!;Wróg na zachodzie!;Düşman batıda!;西の敵だ！ / 西の敵よ！;서쪽이다 적!\", \"interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end;interruption_West_end\", \"interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest;interruption_SouthWest\", \";Враг на Юго-Западе!;Enemy to the South-West!;有敌人在西南边！;Feind im südwesten!;¡Enemigo al suroeste!;Ennemi au sud-ouest !;Nemico a sud-ovest!;Inimigo ao sudoeste!;Wróg na południowym zachodzie!;Düşman güneybatıda!;南西の敵だ！ / 南西の敵よ！;남서쪽이다 적!\", \"interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end;interruption_SouthWest_end\", \"interruption_South;interruption_South;interruption_South;interruption_South;interruption_South;interruption_South;interruption_South;interruption_South;interruption_South;interruption_South;interruption_South;interruption_South;interruption_South\", \";Враг на Юге!;Enemy to the South!;有敌人在南边！;Feind im süden!;¡Enemigo al sur!;Ennemi au sud !;Nemico a sud!;Inimigo ao sul!;Wróg na południu!;Düşman güneyde!;南の敵だ！ / 南の敵よ！;남쪽이다 적!\", \"interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end;interruption_South_end\", \"interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast;interruption_SouthEast\", \";Враг на Юго-Востоке!;Enemy to the South-East!;有敌人在东南边！;Feind im südosten!;¡Enemigo al sureste!;Ennemi au sud-est !;Nemico a sud-est!;Inimigo ao sudeste!;Wróg na południowym wschodzie!;Düşman güneydoğuda!;南東の敵だ！ / 南東の敵よ！;남동쪽이다 적!\", \"interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end;interruption_SouthEast_end\", "; //the string that the iterator will insert
-        
-        foreach(string item in input)
-        {
-            if (item.Contains(findtext))
-            {
-                string newItem = item.Insert(item.IndexOf(findtext), inserttext); //this adds the new string just before the found string. To add it after, simply add ' + findtext.Length' right after 'item.Insert(item.IndexOf(findtext)'
-                yield return newItem;
-            }
-            else
-            {
-                yield return item;
-            }
-        }
-    }
-
-    private static IEnumerable<string> SpeechIterator1(IEnumerable<string> input)
-    {
-        string findtext = "\"Ambush;100;100;"; //the string that the iterator is looking for
-        
-        string inserttext = "\"interruption_East;100;100;100;100;100;100;100;100;100;100;100;100\", \"interruption_NorthEast;100;100;100;100;100;100;100;100;100;100;100;100\", \"interruption_North;100;100;100;100;100;100;100;100;100;100;100;100\", \"interruption_NorthWest;100;100;100;100;100;100;100;100;100;100;100;100\", \"interruption_West;100;100;100;100;100;100;100;100;100;100;100;100\", \"interruption_SouthWest;100;100;100;100;100;100;100;100;100;100;100;100\", \"interruption_South;100;100;100;100;100;100;100;100;100;100;100;100\", \"interruption_SouthEast;100;100;100;100;100;100;100;100;100;100;100;100\", "; //the string that the iterator will insert
-        
-        foreach(string item in input)
-        {
-            if (item.Contains(findtext))
-            {
-                string newItem = item.Insert(item.IndexOf(findtext), inserttext); //this adds the new string just before the found string. To add it after, simply add ' + findtext.Length' right after 'item.Insert(item.IndexOf(findtext)'
-                yield return newItem;
-            }
-            else
-            {
-                yield return item;
-            }
-        }
-    }
-*/
 
         public override void PatchMod()
         {
@@ -510,65 +520,6 @@ b {jmp_end}";
                 .Apply(ContextMenuMouse4AssemblyIterator)
                 .Peek()
                 .Save();
-
-
-            // Msl.AddFunction(ModFiles.GetCode("gml_Object_o_charredtip_arrow_used_Create_0.gml"), "scr_makeCharredAmmo");
-
-
-            /*
-            
-            Msl.AddFunction(ModFiles.GetCode("gml_GlobalScript_scr_GetCardinalDirection.gml"), "scr_getCardinalDirection");
-
-            Msl.LoadGML("gml_GlobalScript_table_log_text")
-                .Apply(LogTextIterator)
-                //.Peek()
-                .Save();
-
-            Msl.LoadGML("gml_GlobalScript_table_speech")
-                .Apply(SpeechIterator0)
-                //.Peek()
-                .Save();
-
-            Msl.LoadGML("gml_GlobalScript_table_speech")
-                .Apply(SpeechIterator1)
-                //.Peek()
-                .Save();
-
-            Msl.LoadGML("gml_Object_o_fogrender_Other_12")
-                .MatchFrom("logFOVTurns = 20") // Finding the line
-                .InsertBelow(ModFiles, "gml_Object_o_fogrender_Other_12_add0.gml")// Inserting the snippet below
-                .Save(); 
-
-            Msl.LoadGML("gml_Object_o_fogrender_Other_12")
-                .MatchFrom("if (state == \"attack\")") // Finding the line
-                .InsertAbove(ModFiles, "gml_Object_o_fogrender_Other_12_add1.gml") // Inserting the snippet above
-                .Save();
-
-            Msl.LoadGML("gml_Object_o_fogrender_Other_12") // Loading a script from the game's files
-                .MatchFrom("scr_actionsLog(\"enemyHostileFOV\", [_name, _name])") // Finding the line
-                .ReplaceBy("scr_actionsLog(\"enemyHostileFOV\", [_nameAndDirection, _name])")// Replacing it
-                .Save();
-
-            Msl.LoadGML("gml_Object_o_fogrender_Other_12")
-                .MatchFrom("scr_actionsLog(\"enemyAlarmFOV\", [_name, _name])")
-                .ReplaceBy("scr_actionsLog(\"enemyAlarmFOV\", [_nameAndDirection, _name])")
-                .Save();
-
-            Msl.LoadGML("gml_Object_o_fogrender_Other_12")
-                .MatchFrom("scr_actionsLog(\"enemyUnawareFOV\", [_name, _name])")
-                .ReplaceBy("scr_actionsLog(\"enemyUnawareFOV\", [_nameAndDirection, _name])")
-                .Save();
-
-
-            Msl.LoadGML("gml_GlobalScript_scr_stateAttack")
-                .MatchFrom("scr_random_speech(\"interruption\", 100, o_player)") // Finding the line
-                .ReplaceBy(ModFiles, "gml_GlobalScript_scr_stateAttack_add0.gml") // Replacing with
-                .MatchFrom("scr_random_speech(\"interruption\", 100, o_player)") // Finding the line once again
-                .ReplaceBy(ModFiles, "gml_GlobalScript_scr_stateAttack_add0.gml") // Replacing with once again
-                //.Peek()
-                .Save();
-
-            */
         }
     }
 }
